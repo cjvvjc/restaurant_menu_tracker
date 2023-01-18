dc0aa600c48c3027f3272b1dffb127d65b5633ae
Chad Volk <cjvolk@gmail.com>
2023-01-18 08:27:09 -0700

    finished adding routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router()
 // const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', indexController.getHome)
+router.post('/q', indexController.getSerach)
 
 module.exports = router
